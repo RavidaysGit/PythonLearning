0833d5a6cb98137f98c257796acdc389f8b2285a
ravi.kumar.narahari <ravi.kumar.narahari@BDC4-L-5B4M0X2.mshome.net>
2020-05-12 17:39:58 +0530

    Python variables learning example

---
+++ PythonLearningPrograms/src/main/python/com/learning/python/VariableExample.py
@@ -0,0 +1,19 @@
+#variables assignments
+name = "ravi"
+age =35
+phone = 987654321
+sal = 34566.34344
+male ='Y'
+age =39
+print(f"Name {name} , Age : {age} , Phone Number : {phone} , Salary :{sal} , Male : {male}") 
+#out put::  Name ravi , Age : 39 , Phone Number : 987654321 , Salary :34566.34344 , Male : Y
+
+movie = "matrix"
+print(movie) # out put    :  matrix
+print(movie[0:]) # out put    :  matrix
+print(movie[0:10]) #out put  : matrix
+print(movie[0]) #out put  : m
+print(movie[0:3]) #out put  : mat
+print(movie[-1]) #out put  : x
+print(movie[2:-2]) #out put  : tr
+
